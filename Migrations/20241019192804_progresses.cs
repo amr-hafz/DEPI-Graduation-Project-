29da2830b4612c8121f2cf6f7cef6422b14d7571
amr-hafz <amrhafz57@gmail.com>
2024-11-22 16:42:05 +0200

    first commit

---
+++ Migrations/20241019192804_progresses.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Online_Courses_2024.Migrations
+{
+    /// <inheritdoc />
+    public partial class progresses : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "progresses",
+                columns: table => new
+                {
+                    LessonId = table.Column<int>(type: "int", nullable: false),
+                    UserId = table.Column<string>(type: "nvarchar(450)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_progresses", x => new { x.UserId, x.LessonId });
+                    table.ForeignKey(
+                        name: "FK_progresses_AspNetUsers_UserId",
+                        column: x => x.UserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_progresses_Lessons_LessonId",
+                        column: x => x.LessonId,
+                        principalTable: "Lessons",
+                        principalColumn: "LessonId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_progresses_LessonId",
+                table: "progresses",
+                column: "LessonId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "progresses");
+
+        }
+    }
+}
---
+++ Migrations/20241019222503_newpro.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Online_Courses_2024.Migrations
+{
+    /// <inheritdoc />
+    public partial class newpro : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+           
+
+            migrationBuilder.AlterColumn<int>(
+                name: "LessonId",
+                table: "progresses",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int")
+                .Annotation("Relational:ColumnOrder", 0);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "UserId",
+                table: "progresses",
+                type: "nvarchar(450)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)")
+                .Annotation("Relational:ColumnOrder", 1);
+
+          
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            
+            migrationBuilder.AlterColumn<int>(
+                name: "LessonId",
+                table: "progresses",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int")
+                .OldAnnotation("Relational:ColumnOrder", 0);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "UserId",
+                table: "progresses",
+                type: "nvarchar(450)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)")
+                .OldAnnotation("Relational:ColumnOrder", 1);
+
+       
+        }
+    }
+}
